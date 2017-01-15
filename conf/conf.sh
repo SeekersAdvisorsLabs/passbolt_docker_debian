5da5f628fab4b6cbe87518318a624001513caa96
JayCyb <dev.cyborg@gmail.com>
2017-01-16 02:56:12 +0800

    default config set docker compose file Warning: first launch requires an interactive user-input, so don't start the container as a daemon

--- conf/conf.sh
+++ conf/conf.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 # Passbolt directory.
-PASSBOLT_DIR=~/projects/passbolt_for_docker_debian
+PASSBOLT_DIR=~/project/passbolt/passbolt_api
 
 # MySQL configuration.
 MYSQL_HOST=localhost
 # Only necessary if we use the local database.
-MYSQL_ROOT_PASSWORD=rootpassword
-MYSQL_USERNAME=passbolt
-MYSQL_PASSWORD=password123
-MYSQL_DATABASE=passbolt
+MYSQL_ROOT_PASSWORD=
+MYSQL_USERNAME=pbuser
+MYSQL_PASSWORD=
+MYSQL_DATABASE=pbdb
 
 # Admin settings.
-ADMIN_USERNAME=admin@passbolt.com
-ADMIN_FIRST_NAME=Admin
-ADMIN_LAST_NAME=Admin
+ADMIN_USERNAME=jbcebe@seekersadvisors.com
+ADMIN_FIRST_NAME=JB
+ADMIN_LAST_NAME=CC
+
+SEND_ANONYMOUS_STATISTICS=n
